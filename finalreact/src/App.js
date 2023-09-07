1b2cc3cd35bd1f2077bf31e2aabd470a9283025d
PureLeaf <volteczx8@gmail.com>
2023-09-07 14:24:12 -0400

    Changement

--- finalreact/src/App.js
+++ finalreact/src/App.js
@@ -2,28 +2,15 @@ import "./App.css";
 import Exam from "./components/Exam";
 
 function App() {
-<<<<<<< HEAD
-  return (
-    <div className="App">
-      <header>
-        <p>Examen Final</p>
-      </header>
-      <p>Mehdi je m'appel</p>
-      <Exam></Exam>
-    </div>
-  );
-=======
-
     return (
         <div className="App">
             <header>
                 <p>Examen Final parce que les examen sont trop null</p>
             </header>
             <Exam></Exam>
-
+            <p>Bug pas mon gars</p>
         </div>
     );
->>>>>>> 4a158097dd933279df14aedca512d426d7340f6e
 }
 
 export default App;
