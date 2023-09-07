e67cfc5270c76dd861baaa1650989e36075e1a68
CaptainMehdi <mehdi.diyani@gmail.com>
2023-09-07 14:10:01 -0400

    mehdi

--- finalreact/src/App.js
+++ finalreact/src/App.js
@@ -1,17 +1,16 @@
-import './App.css';
+import "./App.css";
 import Exam from "./components/Exam";
 
 function App() {
-
-    return (
-        <div className="App">
-            <header>
-                <p>Examen Final</p>
-            </header>
-            <Exam></Exam>
-
-        </div>
-    );
+  return (
+    <div className="App">
+      <header>
+        <p>Examen Final</p>
+      </header>
+      <p>Mehdi je m'appel</p>
+      <Exam></Exam>
+    </div>
+  );
 }
 
 export default App;
