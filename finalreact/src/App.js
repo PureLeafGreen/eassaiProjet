0962f369689fba67665b337f5faa407509e6c883
CaptainMehdi <mehdi.diyani@gmail.com>
2023-09-07 14:25:35 -0400

    mehdi2

--- finalreact/src/App.js
+++ finalreact/src/App.js
@@ -2,15 +2,16 @@ import "./App.css";
 import Exam from "./components/Exam";
 
 function App() {
-    return (
-        <div className="App">
-            <header>
-                <p>Examen Final parce que les examen sont trop null</p>
-            </header>
-            <Exam></Exam>
-            <p>Bug pas mon gars</p>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header>
+        <p>Examen Final parce que les examen sont trop null</p>
+      </header>
+      <Exam></Exam>
+      <p>Bug pas mon gars</p>
+      <p>Le goat </p>
+    </div>
+  );
 }
 
 export default App;
